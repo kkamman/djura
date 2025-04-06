97bb30eb5e8861d8288895dd689b1ee4543f3320
Koen Kamman <koen@kamman.dev>
2025-04-06 20:52:37 +0200

    refactor(components): move theme and utils to separate files

--- apps/showcase/src/app/app.component.ts
+++ apps/showcase/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from 'djura/button';
 import { CommonModule } from '@angular/common';
 import { interval, map } from 'rxjs';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   imports: [CommonModule, RouterModule, ButtonComponent],
@@ -13,5 +14,14 @@ import { interval, map } from 'rxjs';
 export class AppComponent {
   title = 'showcase';
 
+  private readonly renderer = inject(Renderer2);
+  private readonly mediaMatcher = inject(MediaMatcher);
+
   protected readonly progress$ = interval(500).pipe(map((x) => (x % 10) * 10));
+
+  constructor() {
+    if (this.mediaMatcher.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.renderer.addClass(document.body, 'djr-dark');
+    }
+  }
 }
